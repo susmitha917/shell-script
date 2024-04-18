05af3d25c3464427368f13e00df9a8cbbebcab43
susmitha <gonepadususmithareddy917@gmail.com>
2024-04-18 12:09:20 +0530

    function

--- 11-functions.sh
+++ 11-functions.sh
@@ -7,12 +7,12 @@ VALIDATE(){
     echo "What are you doing: $2"
 }
 
-if ( $USERID -ne 0 )
+if [ $USERID -ne 0 ]
 then
-  echo "please run script with root access"
-  exit 1
+    echo "please run script with root access"
+    exit 1
 else
-  echo "You are sper user"
+    echo "You are sper user"
 
 fi
 
@@ -20,4 +20,4 @@ dnf install mysql -y
 VALIDATE $? "Installing MYSQL"
 
 dnf install git -y
-VALIDATE $? "Installing Git"
+VALIDATE $? "Installing MYSQL"
