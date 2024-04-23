aaad02ff057bdf5d540b41f0384f7db0b65e76d0
susmitha <gonepadususmithareddy917@gmail.com>
2024-04-23 16:48:39 +0530

    functions

--- mysql.sh
+++ mysql.sh
@@ -1,33 +1,29 @@
 #!/bin/bash
 
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+#!/bin/bash
+
+source ./common.sh
+
+check_root
+
 echo "Please enter DB password:"
 read -s mysql_root_password
 
-VALIDATE(){
-   if [ $1 -ne 0 ]
-   then
-        echo -e "$2...$R FAILURE $N"
-        exit 1
-    else
-        echo -e "$2...$G SUCCESS $N"
-    fi
-}
+dnf install mysql-server -y &>>$LOGFILE
 
-if [ $USERID -ne 0 ]
+systemctl enable mysqld &>>$LOGFILE
+
+systemctl start mysqld &>>$LOGFILE
+
+#Below code will be useful for idempotent nature
+mysql -h db.daws78s.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+if [ $? -ne 0 ]
 then
-    echo "Please run this script with root access."
-    exit 1 # manually exit if error comes.
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
 else
-    echo "You are super user."
+    echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
 fi
+
 
 
 dnf install mysql-server -y &>>$LOGFILE
