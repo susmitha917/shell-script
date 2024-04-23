a9644f633acbe4f47f32fcdb0e38d17fad0188b4
susmitha <gonepadususmithareddy917@gmail.com>
2024-04-23 12:52:52 +0530

    functions

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -2,9 +2,9 @@
 
 COURSE="Devops from current script"
 
-echo "Before calling other script:$COURSE"
+echo "Before calling other script, course:$COURSE"
 echo "Process ID of current shell-script: $$"
 
 #./16-other-script.sh
 
-echo "After calling other script:$COURSE"
+echo "After calling other script, course:$COURSE"
